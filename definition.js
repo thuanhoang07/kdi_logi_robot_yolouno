0f10f5c37055b537da7b36231b22d1baafbea967
thuanhoang07 <zz.mr.thuan.zz@gmail.com>
2025-07-21 13:36:54 +0700

    Update version print statement in set_custom_pid function

--- definition.js
+++ definition.js
@@ -3194,7 +3194,7 @@ Blockly.Python['set_custom_pid'] = function(block) {
   Blockly.Python.definitions_['import_logi_robot'] = 'import logi_robot';
   
   var code = 'logi_robot.set_custom_pid(' + kp + ', ' + ki + ', ' + kd + ')\n';
-  code += 'print("Version ggggggggggggggggg ")\n'; // Thêm thông báo phiên bản
+  code += 'print("Version hhhhhhhhhhhh ")\n'; // Thêm thông báo phiên bản
   return code;
 };
 
